f67711cf901a20e35e5adde3f4880849b1ff5f09
Jaydeep <jaydeep.dey03@gmail.com>
2023-06-09 13:09:44 +0530

    fixed linting issues

--- src/pages/Resetpassword.tsx
+++ src/pages/Resetpassword.tsx
@@ -2,6 +2,7 @@ import {
     Button,
     Flex,
     FormControl,
+    FormErrorMessage,
     FormLabel,
     Heading,
     Input,
@@ -10,28 +11,9 @@ import {
 } from '@chakra-ui/react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { SDK } from '../appwrite/appwrite-config';
 
 export default function ResetPassword(): JSX.Element {
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [error, setError] = useState('');
 
-    const location = useLocation();
-    const recoveryToken = new URLSearchParams(location.search).get('token');
-
-
-    const handleResetPassword = async () => {
-
-        if (password !== confirmPassword) {
-            setError('Passwords do not match');
-            return;
-        }
-
-        // SDK.updateRecovery()
-    };
 
     return (
         <Flex
@@ -58,35 +40,40 @@ export default function ResetPassword(): JSX.Element {
                     })}
                     onSubmit={
                         (value, action) => {
-
+                            console.log(value)
+                            action.resetForm();
                         }
                     }
                 >
                     {formik => (
                         <>
-                            <FormControl id="password" isRequired>
-                                <FormLabel>Password</FormLabel>
-                                <Input
-                                    placeholder="your-email@example.com"
-                                    _placeholder={{ color: 'gray.500' }}
-                                    type="password"
-                                />
-                            </FormControl>
-                            <FormControl id="confirmpassword" isRequired>
-                                <FormLabel>Confirm Password</FormLabel>
-                                <Input type="password" />
-                            </FormControl>
-                            <Stack spacing={6}>
-                                <Button
-                                    type="submit"
-                                    bg={'blue.400'}
-                                    color={'white'}
-                                    _hover={{
-                                        bg: 'blue.500',
-                                    }}>
-                                    Submit
-                                </Button>
-                            </Stack>
+                            <form onSubmit={formik.handleSubmit}>
+                                <FormControl id="password" isRequired
+                                    isInvalid={!!formik.errors.password && formik.touched.password}>
+                                    <FormLabel>Password</FormLabel>
+                                    <Input
+                                        placeholder="your-email@example.com"
+                                        _placeholder={{ color: 'gray.500' }}
+                                        type="password"
+                                    />
+                                    <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+                                </FormControl>
+                                <FormControl id="confirmpassword" isRequired>
+                                    <FormLabel>Confirm Password</FormLabel>
+                                    <Input type="password" />
+                                </FormControl>
+                                <Stack spacing={6}>
+                                    <Button
+                                        type="submit"
+                                        bg={'blue.400'}
+                                        color={'white'}
+                                        _hover={{
+                                            bg: 'blue.500',
+                                        }}>
+                                        Submit
+                                    </Button>
+                                </Stack>
+                            </form>
                         </>
                     )}
                 </Formik>
